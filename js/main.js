3c96129740820646c2f8805f19397b0435558862
Richard Chan <richardchanme@gmail.com>
2017-08-10 23:59:06 -0700

    working on totalSum logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 function addItem() {
+	// const category = document.getElementById('').value;
 	const comments = document.getElementById('comments').value;
 	const amount = document.getElementById('amount').value;
 	let dollar = "$";
 	// if user input is empty, don't add anything to to-do list
-	if (comments != '') {
+	if (comments != '' && amount != '') {
 	const list = document.getElementById('item-list');
 	const text = document.createElement('li');
-	text.className = 'list-group-item col-md-12';
+	text.className = 'list-group-item';
 
 	text.innerHTML = comments + " " + dollar + amount;
 	list.appendChild(text);
 	}
 	// clears out input form after content has been added
 	document.getElementById('comments').value = '';
+	document.getElementById('amount').value = '';
+
+	return totalSum();
 	
 	console.log(comments + " " + dollar + amount);
 }
@@ -21,6 +25,11 @@ function addItem() {
 document.getElementById('add').onclick = function() {
 	addItem(); // calls addItem function
 };
+
+function totalSum() {
+	const totalAmount = document.getElementById('total');
+	return totalAmount;
+}
 
 // create delete button next to new to-do item
 function deleteItem(item) {
