e43c778263d06d9c6435ff325c729c2299d38076
Richard Chan <richardchanme@gmail.com>
2017-08-11 21:52:50 -0700

    total amount shows on browser

--- js/main.js
+++ js/main.js
@@ -2,36 +2,29 @@ function addItem() {
 	// const category = document.getElementById('').value;
 	const comments = document.getElementById('comments').value;
 	const amount = document.getElementById('amount').value;
-	let dollar = "$";
-
+	const totalAmount = document.getElementById('total');
+	
 	// if user input is empty, don't add anything to to-do list
 	if (comments != '' && amount != '') {
 	const list = document.getElementById('item-list');
 	const text = document.createElement('li');
 	text.className = 'list-group-item';
 
-	text.innerHTML = comments + " " + dollar + amount;
+	text.innerHTML = comments + " $" + amount;
 	list.appendChild(text);
 	}
 	// clears out input form after content has been added
 	document.getElementById('comments').value = '';
 	document.getElementById('amount').value = '';
-
-	return totalSum();
+	totalAmount.innerHTML = '$' + amount;
 	
-	console.log(comments + " " + dollar + amount);
+	console.log(comments + " $" + amount);
 }
 
 // adds functionality to Add button
 document.getElementById('add').onclick = function() {
 	addItem(); // calls addItem function
 };
-
-function totalSum() {
-	const totalAmount = document.getElementById('total');
-	totalAmount.innerHTML = amount;
-	return totalAmount;
-}
 
 // create delete button next to new to-do item
 function deleteItem(item) {
